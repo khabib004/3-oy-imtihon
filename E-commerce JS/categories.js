3b48a93ff3893f08f48e5bd24edd46750d3ad1a3
khabib004 <norboyevxabibjon@example.com>
2023-08-05 16:46:59 +0500

    imtihon

--- E-commerce JS/categories.js
+++
@@ -1,6 +0,0 @@
-const categoryMenuToggleBtn = document.querySelector( "header button" );
-const categoriesMenuDropdown = document.querySelector( ".categories-menyu" );
-
-categoryMenuToggleBtn.addEventListener( "click", () => {
-  categoriesMenuDropdown.classList.toggle( "categories-toggle" );
-} );
