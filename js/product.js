b806fa4b1c47781d4a4e9987ff0f3dfe5f39d498
khabib004 <norboyevxabibjon@example.com>
2023-08-08 12:27:17 +0500

    imtihon

--- js/product.js
+++ js/product.js
@@ -4,6 +4,25 @@ const totalProducts = document.querySelector( ".total-products" );
 
 let search = "";
 
+function getProductCard( pr ) {
+  return `
+    <div class="product-card">
+      <div class="product-card-body">
+        <img
+          src="${pr.images[ 0 ]}"
+          alt="${pr.name}"
+        />
+      </div>
+      <div class="product-card-footer">
+        <h3><a href="product.html">${pr.name}</a></h3>
+        <p> ${pr.description} </p>
+        <p><i> ${pr.price} </i></p>
+        <button><b> Add to the cart </b></button>
+      </div>
+    </div>
+  `;
+}
+
 function getProducts() {
   let results = products.filter(
     ( pr ) =>
